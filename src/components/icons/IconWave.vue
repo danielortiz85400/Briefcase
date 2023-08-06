1a3e674c0ecc236107ed58b5fec663702e91d930
danielortiz85400 <danielortizespitia8@gmail.com>
2023-08-05 19:13:25 -0500

    create: header, social-media, personal-desc, personal-info, skills

---
+++ src/components/icons/IconWave.vue
@@ -0,0 +1,14 @@
+<template>
+    <svg width="100%" height="100%" id="svg" viewBox="0 0 1440 390" xmlns="http://www.w3.org/2000/svg"
+        class="transition duration-300 ease-in-out delay-150">
+        <defs>
+        <linearGradient id="gradient" x1="0%" y1="50%" x2="100%" y2="50%">
+            <stop offset="5%" stop-color="#7bdcb5"></stop>
+            <stop offset="95%" stop-color="#8ED1FC"></stop>
+        </linearGradient>
+    </defs>
+    <path
+        d="M 0,400 C 0,400 0,200 0,200 C 119,193.96428571428572 238,187.92857142857142 364,211 C 490,234.07142857142858 623.0000000000001,286.25 754,268 C 884.9999999999999,249.75 1014,161.07142857142856 1128,138 C 1242,114.92857142857143 1341,157.46428571428572 1440,200 C 1440,200 1440,400 1440,400 Z"
+        stroke="none" stroke-width="0" fill="url(#gradient)" fill-opacity="1"
+        class="transition-all duration-300 ease-in-out delay-150 path-0"></path>
+</svg></template>
